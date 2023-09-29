4a73f87a90df2c757720babb6845bbe17064daff
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-29 09:57:14 -0300

    lo hicimos responsive ponele

--- 1parternexpo/src/screens/Perfil.js
+++ 1parternexpo/src/screens/Perfil.js
@@ -140,6 +140,7 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    width: 200
   },
 });
 
--- 1parternexpo/src/screens/Register.js
+++ 1parternexpo/src/screens/Register.js
@@ -96,6 +96,7 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    width: 200
   }
 });
 
